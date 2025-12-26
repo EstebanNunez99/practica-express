dac305ffea0f8f010e51da6fd4b7822d64f62eb3
Esteban <nunezesteban750@gmail.com>
2025-12-26 20:19:01 -0300

    add: route POST to API

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,31 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.use(express.json())
+
+//---- Rutas ----
 app.get('/', (req, res) => {
     res.send('Welcome to my API build with Express ')
 })
+
+app.get('/api/products', (req, res)=>{
+    const products = [
+        {id: 1, name:"product 1", price:300},
+        {id: 2, name:"product 2", price:200}
+    ]
+    res.json(products)
+})
+
+app.post('/api/products', ( req, res )=>{
+    const newProduct = req.body
+    console.log('product received', newProduct)
+
+    res.status(201).json({
+        msg: 'product successfully created',
+        product : newProduct
+    })
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server listen Successfully on http://localhost:${PORT}`)
